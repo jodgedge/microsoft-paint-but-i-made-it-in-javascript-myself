0d8bf6c67df6cbd50516c2ef2f906a675abbe0d9
jodgedge <78231510+jodgedge@users.noreply.github.com>
2021-03-15 17:45:17 -0400

    README.js
    DONT OPEN THE READ ME PLEASE

---
+++ README.js
@@ -0,0 +1,9 @@
+Title:("Painter")
+Developer:("jodgedge")
+P A I N T E R
+A Demake of Microsoft Paint, a image editor that comes with Windows.
+FEATURES: Mostly mapped to Keyboard Shortcuts.
+Contains a whooping 5 COLORS!!!!!😍😍😍😍😍😍😍😍😍😍😍
+There is some secrets😮
+totally better than MS Paint😎
+There is no installation required. Just one HTML file:)
